5b850070c5a0061effa01e300c278a3cf750b1f2
janegould <60157546+janegould@users.noreply.github.com>
2020-03-18 22:16:59 -0400

    Add files via upload

--- poke.js
+++ poke.js
@@ -1,15 +1,76 @@
-var queryURL = "https://pokeapi.co/api/v2/pokemon/"
+
+//on start click - show three pokemon
+
+var queryURL1 = "https://pokeapi.co/api/v2/pokemon/jigglypuff"
 
   $("#startBtn").on("click", function(){
     $.ajax({
-      url: queryURL,
+      url: queryURL1,
       method: "GET"
     }).then(function (response) {
-      console.log(response, "Im here!!!!");
+        var image1 = response.sprites.front_default;
+        var imageElement = document.createElement("img");
+        var btn1 = document.createElement("button");
+        btn1.setAttribute("class", "startBtn");
+
+        imageElement.setAttribute("src", image1);
+        btn1.append(imageElement);
+        var div = document.getElementById("pokemon");
+        div.append(btn1);
+
     })
     })
 
-    //on start click - show three pokemon
+    var queryURL2 = "https://pokeapi.co/api/v2/pokemon/ditto"
+
+  $("#startBtn").on("click", function(){
+    $.ajax({
+      url: queryURL2,
+      method: "GET"
+    }).then(function (response) {
+    var image2 = response.sprites.front_default;
+    var imageElement = document.createElement("img");
+    var btn2 = document.createElement("button");
+    btn2.setAttribute("class", "startBtn");
+
+    imageElement.setAttribute("src", image2);
+    btn2.append(imageElement);
+    var div = document.getElementById("pokemon");
+    div.append(btn2);
+
+      
+    })
+    })
+
+    var queryURL3 = "https://pokeapi.co/api/v2/pokemon/gengar"
+
+    $("#startBtn").on("click", function(){
+      $.ajax({
+        url: queryURL3,
+        method: "GET"
+      }).then(function (response) {
+      var image3 = response.sprites.front_default;
+      var imageElement = document.createElement("img");
+      var btn3 = document.createElement("button");
+      btn3.setAttribute("class", "startBtn");
+
+
+      imageElement.setAttribute("src", image3);
+      btn3.append(imageElement);
+      var div = document.getElementById("pokemon");
+      div.append(btn3);
+       
+      })
+      })
+    
+    var startBtns = document.getElementsByClassName("startBtn");
+      for (var i = 0; i< startBtns.length; i++){
+          console.log("test");
+          startBtns[i].addEventListener("click", function(){
+              console.log("something");
+          })
+      }
+
 
     //when the user chooses one, then load pre-made maze 
 
