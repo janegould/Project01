d1c209dc66fc995ea98075b5ab5af0b678a3631d
caioebueno <caioebueno@gmail.com>
2020-03-23 16:36:53 -0400

    Add files via upload

--- poke.js
+++ poke.js
@@ -1,6 +1,7 @@
 var queryURL1 = "https://pokeapi.co/api/v2/pokemon/jigglypuff"
 var pokeImg;
 var cellNumber = 1;
+var url = "https://www.googleapis.com/youtube/v3/videos?id=-PlAg8R9TG4&key=AIzaSyCJ1iJthOd27F_OOEKx_-hJ8S07jCBDZB0&part=snippet,statistics";
 
   $("#startBtn").on("click", function start(){
     document.getElementById("pokemon").innerHTML = "";
@@ -71,7 +72,22 @@ var cellNumber = 1;
             var cell1 = document.getElementById("cell1");
             cell1.append(spriteDiv);
             pokeImg = img.getAttribute("src");
+            
+            $.ajax({
+              url: url,
+              method: "GET"
+            }).then(function(response){
+              
+              console.log(response);
+              var youUrl = "https://www.youtube.com/embed/" + response.items[0].id + "?autoplay=1";
+              var iframe = document.getElementById("iframe");
+              iframe.setAttribute("src", youUrl);
+              
+            
+            })
+
             moveSprite();
+            startTimer();
 
         })
       } 
@@ -160,7 +176,6 @@ var cellNumber = 1;
       
         if(document.getElementById("cell36").firstChild == document.getElementById("sprite")){
 
-          console.log("win");
           var body = document.querySelector("body");
           body.setAttribute("class", "end");
           var x = document.getElementById("x");
@@ -171,8 +186,9 @@ var cellNumber = 1;
           img.style.width = "300px";
           var winDiv = document.getElementById("win");
           winDiv.style.visibility = "visible";
-
           body.append(img);
+
+          setScore();
 
 
         }
@@ -180,3 +196,101 @@ var cellNumber = 1;
 
         }
       }
+      
+    var timeLeft = 60;
+   
+    function startTimer(){
+
+      
+      var elem = document.getElementById('timer');
+    
+      var timerId = setInterval(countdown, 1000);
+
+      function countdown() {
+        if (timeLeft == 0) {
+          clearTimeout(timerId);
+          score()
+        } else {
+          elem.innerHTML = timeLeft + ' seconds remaining';
+          timeLeft--;
+        }
+      }
+
+    }
+    var score  = 0;
+
+    function setScore(){
+      score = timeLeft * 100;
+      var div = document.getElementById("score");
+      div.innerHTML = score; 
+
+    }
+
+    var nameArray = "caio";
+    var scoreArray = "10000000";
+
+    function pushScore(){
+      window.localStorage.setItem("name", nameArray);
+      window.localStorage.setItem("score", scoreArray);
+
+    }
+
+    function getScore(){
+      
+      var nameLocal = window.localStorage.getItem("name");
+      var scoreLocal = window.localStorage.getItem("score");
+
+      if(nameLocal == null){
+
+      }else{
+      nameArray = nameLocal;
+      scoreArray = scoreLocal;
+      }
+    }
+    getScore();
+
+
+
+    function upScore(){
+      
+      var name = document.getElementById("nameInput").value;
+      nameArray = nameArray + "," +  name;
+      scoreArray = scoreArray + "," +  score;
+      pushScore();
+      getScore();
+
+    }
+    function viewScore(){
+      
+      var xDiv = document.getElementById("x");
+      var winDiv = document.getElementById("win");
+      var scoreDiv = document.getElementById("viewScoreDiv");
+  
+      scoreDiv.setAttribute("style", "visibility: visible");
+      xDiv.setAttribute("style", "visibility: hidden;");
+      winDiv.setAttribute("style", "visibility: hidden;");
+
+      var nameString = localStorage.getItem("name");
+      var scoreString = localStorage.getItem("score");
+      var nameArray = nameString.split(",");
+      var scoreArray = scoreString.split(",");
+
+      var ul = document.getElementById("scoreUl");
+      ul.innerHTML = "";
+      
+      for(i = 0; i < nameArray.length; i++){
+        var li = document.createElement("li");
+        li.innerHTML = nameArray[i] + " " + scoreArray[i];
+        ul.append(li);
+
+      }
+    
+    }
+    function hideScore(){
+      var scoreDiv = document.getElementById("viewScoreDiv");
+      var gameDiv = document.getElementById("x");
+      scoreDiv.setAttribute("style", "visibility: hidden");
+      gameDiv.setAttribute("style", "visibility: visible");
+    }
+    
+    
